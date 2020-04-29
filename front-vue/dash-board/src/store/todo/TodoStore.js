d83b57e0d261d2a4d0f56e3afe9958a11cae6355
kuh for mac <dkrnl1318@naver.com>
2020-04-29 18:14:03 +0900

    feat:Todo store

--- front-vue/dash-board/src/store/todo/TodoStore.js
+++ front-vue/dash-board/src/store/todo/TodoStore.js
@@ -8,23 +8,59 @@
  *  mutation = { functionName(){} },
  */
 
-
+import { todoService } from "../../services";
 
 
 const state = {
+    todoList : [],
+    selectedTodoItem : {}
 
 }
 
 const getters = {
+    getTodoList : function(state){
+        return state.todo.todoList;
+    },
+    getSeltedTodoItem : function(state,){
 
+    }
 }
 
 const actions = {
+    todoListDownload : function(context){
+        let todoList = todoService.getTodoList();
+        context.commit('setTodoList', { todoList });
+    },
+    todoItemUpdate : function (context , payload){
+
+        console.log(payload)
+        let item = payload.todoItem;
+        todoService.todoItemUpdate('', item );
+        context.commit('updateTodoItem' , { todoItem : item });
+    }
 
 }
 
 const mutation = {
+    setTodoList : function(state ,payload){
+        state.todo.todoList = payload.todoList;
+    },
+    addTotoItem : function(state , payload){
+        state.todo.todoList.push(payload.todoItem);
+    },
+    updateTodoItem : function(state , payload){
 
+        let todoList = [ ... state.todo.todoList];
+        state.todo.todoList = todoList.map(t=> {
+            if( t.id === payload.todoItem.id ){
+                return {
+                    ...payload,
+                }
+            }
+
+            return t;
+        });
+    }
 }
 
 
