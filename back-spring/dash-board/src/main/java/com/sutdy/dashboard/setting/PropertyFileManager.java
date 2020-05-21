d7f1b9a53e5e829b9e2dc28b9e9c51df52551417
의현(Home) <dkrnl1318@gmail.com>
2020-05-21 20:36:00 +0900

    feat:Temp

--- back-spring/dash-board/src/main/java/com/sutdy/dashboard/setting/PropertyFileManager.java
+++ back-spring/dash-board/src/main/java/com/sutdy/dashboard/setting/PropertyFileManager.java
@@ -7,8 +7,12 @@ import org.springframework.context.annotation.PropertySource;
  * @since 2020.05.04
  */
 public class PropertyFileManager {
+
+    private  static  String Mode = "dev";
+
     public final static String ERROR_MGS_PROP = "classpath:application-ErrorMsg.properties";
     public final static String DEV_DATA_SOURCE_PROP = "classpath:application-db-dev.properties";
     public final static String REL_DATA_SOURCE_PROP = "classpath:application-db-rel.properties";
+
 
 }
