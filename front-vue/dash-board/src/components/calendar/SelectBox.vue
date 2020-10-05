a0943e2e044c2eb01e34f8eec3e74174f8d89ba9
KimUihyeon <dkrnl1318@naver.com>
2020-10-05 23:33:17 +0900

    feat:SelectedBox Components

---
+++ front-vue/dash-board/src/components/calendar/SelectBox.vue
@@ -0,0 +1,25 @@
+<template>
+    <span>
+        <el-option
+            v-for="item in items"
+            :key="item.id"
+            :label="item.title"
+            :value="item.id">
+                                        
+            <span style="float: left">{{ item.title }}</span>
+            <span style="float: right; color: #8492a6; font-size: 13px">{{ item.title }}</span>  
+        </el-option>
+    </span>
+</template>
+
+<script>
+const name = 'selectBox';
+const props = { 
+    items : Array
+}
+
+export default {
+    name,
+    props,
+}
+</script>
